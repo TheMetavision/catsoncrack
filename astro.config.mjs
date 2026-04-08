f97cf2bbfd574e3e21299e779143f06e08bf83aa
TheMetavision <contact@themetavision.co.uk>
2026-04-08 17:13:20 +0100

    Wire character page to Sanity CMS - 21 characters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://catsoncrack.co.uk',
   output: 'static',
-  adapter: netlify(),
   integrations: [react(), sitemap()],
-  vite: {
-    ssr: {
-      noExternal: ['@sanity/client', '@sanity/image-url'],
-    },
-  },
+  vite: { ssr: { noExternal: ['@sanity/client', '@sanity/image-url'] } }
 });
